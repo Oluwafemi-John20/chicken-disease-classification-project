d8d1783a17f643d747f3d6e274267c2032be5363
Oluwafemi-John20 <adeyemofemi19@gmail.com>
2023-06-08 17:20:52 +0100

    requirements added

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "cnnClassifier"
+AUTHOR_USER_NAME = "Oluwafemi"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "adeyemofemi19@gmail.com"
+
+setuptools.setup(
+    name = SRC_REPO,
+    version = __version__,
+    author = AUTHOR_USER_NAME,
+    author_email  = AUTHOR_EMAIL,
+    description = "A small python pacckage that classifies chicken disease.",
+    long_description = long_description,
+    long_description_content = "text/markdown",
+    url = f"https://github.com/Oluwafemi-John/chicken-disease-classification-poject",
+    project_urls = {
+        "Bug Tracker": f"https://github.com/Oluwafemi-John/chicken-disease-classification-poject/issues"
+    },
+    package_dir = {"": "src"},
+    packages = setuptools.find_packages(where = "src")
+)
