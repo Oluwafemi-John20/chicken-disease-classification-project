ccb8cc69b164f9ef9ae7b11a8124c26ce618eff6
Ayodele Adeyemo <christad92@gmail.com>
2023-07-12 21:23:04 +0100

    prepare base model completed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 
@@ -10,6 +11,19 @@ if __name__ == '__main__':
         obj.main()
         logger.info(f">>>>>> stage{STAGE_NAME} completed <<<<<<\n\nx==============x")
 
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+
+STAGE_NAME = "prepare_base_model"
+if __name__ == "__main__":
+    try:
+        logger.info(f"*************************")
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>>>> stage {STAGE_NAME} completed <<<<<<<<<\n\nx=========x")
     except Exception as e:
         logger.exception(e)
         raise e
--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -1,4 +1,4 @@
-from cnnClassifier.config.configuration import ConfigManager
+from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier import logger
 
@@ -8,7 +8,7 @@ class DataIngestionTrainingPipeline:
     def __init__(self):
         pass
     def main(self):
-        config= ConfigManager()
+        config= ConfigurationManager()
         data_ingestion_config= config.get_data_ingestion_config()
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
